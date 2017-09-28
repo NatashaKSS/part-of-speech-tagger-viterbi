3d5cba06680a8964315b3b6eb74908d4b4bad3b9
NatashaKSS <xnagol@gmail.com>
2017-09-28 13:24:02 +0800

    Added skeleton class for computing HMM probabilities

---
+++ HMMProbGenerator.py
@@ -0,0 +1,19 @@
+# Import standard modules
+import string
+import re
+
+class HMMProbGenerator():
+  def __init__(self):
+    print("HMMProbGenerator instantiated...")
+
+  """
+  Generate emission & transition probabilities from a Dictionary of bigram
+  counts and tag counts
+
+  bigram_counts    Dictionary of bigram counts
+
+  return           
+  """
+  def generate_probs(self):
+    print('generate probs')
+    return 'probs';
