13796e0cda0f0002e66f56e0854f5442e18bc45a
NatashaKSS <xnagol@gmail.com>
2017-09-29 01:31:07 +0800

    Added method to initialize viterbi memo

--- run_tagger.py
+++ run_tagger.py
@@ -1,19 +1,58 @@
 # Import standard modules
 import sys
+import math
+import pickle
 
-from Tokenizer import Tokenizer;
+# Import custom modules
+from Tokenizer import Tokenizer
+from PennTreebankPOSTags import POS_TAGS, START_MARKER, END_MARKER
 
 #=====================================================#
 # RUNNING THE POS TAGGER
 #=====================================================#
 class POSTagger():
   def __init__(self):
-    print('__init__')
+    # Initialize constants
+    MODEL = self.load_model()
+    self.PROB_TAG_GIVEN_TAG = MODEL[0]
+    self.PROB_WORD_GIVEN_TAG = MODEL[1]
+
     self.tokenizer = Tokenizer()
 
-  def load_document(self):
+  def run(self):
+    print("-- RUNNING THE PART OF SPEECH TAGGER --")
+    sentences = self.load_document_as_sentences()
+    sen_as_tokens_list = [self.tokenizer.tokenize_test_document(sentence) for sentence in sentences]
+    sen_as_tokens_list = [sen_tokens for sen_tokens in sen_as_tokens_list if sen_tokens != []] # remove any empty lists due to empty sentences
+
+    for sen_tokens in sen_as_tokens_list:
+      viterbi_memo = self.initialize_viterbi_memo_matrix(sen_tokens)
+      print(viterbi_memo['NN'])
+
+  #=====================================================#
+  # VITERBI ALGORITHM
+  #=====================================================#
+  def initialize_viterbi_memo_matrix(self, tokens):
+    memo = {}
+    for postag in POS_TAGS:
+      for token in tokens:
+        if postag not in memo:
+          memo[postag] = {}
+        memo[postag][token] = float('-inf')
+    return memo
+
+  def get_POS_tags(self, tokens):
+    print(tokens)
+
+  #=====================================================#
+  # LOAD FILES & INITIALIZE MODEL
+  #=====================================================#
+  def load_document_as_sentences(self):
     DATA_TEST = open(PATH_TO_DATA_TEST).read()
-    print(self.tokenizer.tokenize_document(DATA_TEST))
+    return self.tokenizer.generate_sentences_from_test_document(DATA_TEST)
+
+  def load_model(self):
+    return pickle.load(open(PATH_TO_DATA_MODEL, 'rb'))
 
 #=====================================================#
 # EXECUTION OF PROGRAM
@@ -24,5 +63,4 @@ PATH_TO_DATA_TEST_LABELLED = sys.argv[3]
 
 print("Test data:", PATH_TO_DATA_TEST + ", Model:", PATH_TO_DATA_MODEL + ", labelled test data:", PATH_TO_DATA_TEST_LABELLED)
 
-tagger = POSTagger()
-tagger.load_document()
+tagger = POSTagger().run()
