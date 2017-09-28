c923b9b3f86232d622a7e2bc8f2140b30ee4be1c
NatashaKSS <xnagol@gmail.com>
2017-09-29 01:30:18 +0800

    Added methods for tokenizing test document

--- Tokenizer.py
+++ Tokenizer.py
@@ -15,7 +15,50 @@ class Tokenizer():
   #=====================================================#
   # TOKENIZER FOR UNSEEN SENTENCES
   #=====================================================#
-  def get_test_data_tokens(self, doc_string):
+  def generate_sentences_from_test_document(self, doc_string):
+    sentences = self.get_sentences(doc_string)
+    return self.insert_start_end_sentence_tokens(sentences)
+
+  """
+  Tokenizes a string for the Test set.
+
+  doc_string    String of the document to tokenize
+
+  return    List of tokens split according to the RegEX rules
+  """
+  def tokenize_test_document(self, doc_string):
+    doc_tokens = self.get_test_data_tokens(doc_string)
+    return self.remove_empty_sentence_at_end(doc_tokens)
+
+  # Removes the 'S' and 'E' at the back of [...'<S>', '', '<E>'].
+  # This occurs when we have an extra sentence at the last line in a file
+  def remove_empty_sentence_at_end(self, tokens):
+    LENGTH_TOKENS = len(tokens)
+    if LENGTH_TOKENS >= 3:
+      if tokens[LENGTH_TOKENS - 3] == START_MARKER and tokens[LENGTH_TOKENS - 1] == END_MARKER:
+        # in the format of [...'<S>', '', '<E>']
+        return tokens[0 : -3]
+
+    # 1-word tokens will never have empty sentences at the end
+    return tokens
+
+  # Sandwich START & END of sentence markers between each sentence in a list of sentences
+  # Any leading and trailing space between sentences will be removed too
+  # Unlike the tokenizer for labelled corpus, we DON'T add the labelled START & END MARKER
+  def insert_start_end_sentence_tokens(self, sentences):
+    START_SENTENCE = START_MARKER + ' '
+    END_SENTENCE = ' ' + END_MARKER
+    return [START_SENTENCE + sentence.strip(' ') + END_SENTENCE for sentence in sentences]
+
+  # Terms in test data are separated by spaces, so this splits them
+  def get_test_data_tokens(self, doc_string_with_S_E):
+    return doc_string_with_S_E.split(' ')
+
+  # == UNUSED ==
+  # Tokenizes some raw corpus based on these RegEX rules.
+  # Since the test set is in a well-defined format, for this assignment, we need
+  # not use these rules. Splitting on ' ' is fine.
+  def get_test_data_tokens_UNUSED(self, doc_string):
     return re.findall(r"[\w]+|[.,!?;\(\)\[\](...)('s)('d)(n't)('ll)('S)('D)(N'T)('LL)]+", doc_string)
 
   #=====================================================#
