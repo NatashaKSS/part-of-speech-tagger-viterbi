eaccabc5ed8277e3a25e5f3f0f70cb94e2e38cb7
NatashaKSS <xnagol@gmail.com>
2017-10-07 23:37:09 +0800

    Moved POSTagModelTrainer class to its own py file

---
+++ POSTagModelTrainer.py
@@ -0,0 +1,39 @@
+# Import standard modules
+import sys
+import pickle
+
+# Import custom modules
+from Tokenizer import Tokenizer
+from HMMProbGenerator import HMMProbGenerator
+
+#=====================================================#
+# TRAINING THE POS TAGGER
+#=====================================================#
+class POSTagModelTrainer():
+  def __init__(self, PATH_TO_DATA_TRAIN, validate=False):
+    if validate:
+      print("== CROSS VALIDATION MODE ==")
+    else:
+      # Set up tokenizer before everything else
+      self.tokenizer = Tokenizer()
+
+      # Initialized constants
+      self.DATA_TRAIN = open(PATH_TO_DATA_TRAIN).read()
+      self.LIST_OF_WORD_POSTAG_PAIRS = self.load_training_data()
+
+  def train(self):
+    list_of_labelled_words = self.LIST_OF_WORD_POSTAG_PAIRS # [['its', 'PRP$'], ['to', 'TO'] ...]
+    model = HMMProbGenerator(list_of_labelled_words).generate_probs()
+    pickle.dump(model, open('model_file', 'wb'))
+    print("--- FINISHED TRAINING...MODEL SAVED IN model_file ---")
+
+  """
+  Loads the training data in-memory and tokenizes it.
+
+  return        List of strings in the format
+                ['<word1>/<pos_tag1>', '<word2>/<pos_tag2>', ...]
+  """
+  def load_training_data(self):
+    list_of_str_postag = self.tokenizer.tokenize_document(self.DATA_TRAIN)
+    list_of_word_postag_pairs = self.tokenizer.get_pairs_of_word_tags(list_of_str_postag)
+    return list_of_word_postag_pairs
--- build_tagger.py
+++ build_tagger.py
@@ -3,47 +3,7 @@ import sys
 import pickle
 
 # Import custom modules
-from PennTreebankPOSTags import POS_TAGS
-from Tokenizer import Tokenizer
-from HMMProbGenerator import HMMProbGenerator
-
-#=====================================================#
-# TRAINING THE POS TAGGER
-#=====================================================#
-class POSTagModelTrainer():
-  def __init__(self):
-    # Set up tokenizer before everything else
-    self.tokenizer = Tokenizer()
-
-    # Initialized constants
-    self.DATA_TRAIN = open(PATH_TO_DATA_TRAIN).read()
-    self.LIST_OF_WORD_POSTAG_PAIRS = self.load_training_data()
-
-  def train(self):
-    list_of_labelled_words = self.LIST_OF_WORD_POSTAG_PAIRS # [['its', 'PRP$'], ['to', 'TO'] ...]
-    model = HMMProbGenerator(list_of_labelled_words).generate_probs()
-    pickle.dump(model, open('model_file', 'wb'))
-    print("--- FINISHED TRAINING...MODEL SAVED IN model_file ---")
-
-  """
-  Loads the training data in-memory and splits each token on ' '
-
-  return        List of strings in the format ['<word1>/<pos_tag1>', '<word2>/<pos_tag2>', ...]
-  """
-  def load_training_data(self):
-    list_of_str_postag = self.tokenizer.tokenize_document(self.DATA_TRAIN)
-    list_of_word_postag_pairs = self.tokenizer.get_pairs_of_word_tags(list_of_str_postag)
-    return list_of_word_postag_pairs
-
-
-  #=====================================================#
-  # DEBUGGING HELPER FUNCTIONS
-  #=====================================================#
-  def findTokenPair(self, word1):
-    for index, postag_pairs in enumerate(self.LIST_OF_WORD_POSTAG_PAIRS):
-      if postag_pairs[0] == word1:
-        return [index, postag_pairs[0], postag_pairs[1]]
-    return None
+from POSTagModelTrainer import POSTagModelTrainer
 
 #=====================================================#
 # EXECUTION OF PROGRAM
@@ -54,4 +14,4 @@ PATH_TO_DATA_MODEL = sys.argv[3]
 
 print("Training data:", PATH_TO_DATA_TRAIN + ", Devt Data:", PATH_TO_DATA_DEVT + ", Model file:", PATH_TO_DATA_MODEL)
 
-model = POSTagModelTrainer().train()
+POSTagModelTrainer(PATH_TO_DATA_TRAIN).train()
