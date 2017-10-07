9de4456b9705021556832ebb93a032de1b4eb8b3
NatashaKSS <xnagol@gmail.com>
2017-10-07 16:25:09 +0800

    Implemented viterbi algorithm with fixes * Implemented backpointer traversal * Improved documentation
    Fixes:
    * Fixed dealing with log probabilties
    * Fixed having a back ptr to the 1st tag, which is undefined anyways

--- run_tagger.py
+++ run_tagger.py
@@ -7,27 +7,36 @@ import pickle
 from Tokenizer import Tokenizer
 from PennTreebankPOSTags import POS_TAGS, START_MARKER, END_MARKER
 
+# Define constants
+UNK = '<UNK>'
+
 #=====================================================#
 # RUNNING THE POS TAGGER
 #=====================================================#
 class POSTagger():
   def __init__(self):
-    # Initialize constants
     MODEL = self.load_model()
+
+    # Matrix representing P(t_i | t_i-1), where rows: t_i-1, cols: t_i
     self.PROB_TAG_GIVEN_TAG = MODEL[0]
+
+    # Matrix representing P(w_i | t_i),  where rows: t_i, cols: w_i
     self.PROB_WORD_GIVEN_TAG = MODEL[1]
+
+    # List of seen words
+    self.VOCAB_WORDS = self.PROB_WORD_GIVEN_TAG['NN'].keys()
 
     self.tokenizer = Tokenizer()
 
   def run(self):
     print("-- RUNNING THE PART OF SPEECH TAGGER --")
     sentences = self.load_document_as_sentences()
+    sen_as_tokens_list = self.generate_tokens_for_test_doc_sentences(sentences, self.VOCAB_WORDS)
 
-    # TODO: We process each sentence as a separate test doc to run the tagger on
-    # (might not be accurate especially for shorter articles)
-    sen_as_tokens_list = self.generate_tokens_for_test_doc_sentences(sentences)
-    for sen_tokens in [sen_as_tokens_list[0]]:
-      print(self.tag(sen_tokens))
+    for sen_tokens in sen_as_tokens_list:
+      print('\n', self.tag(sen_tokens), '\n========')
+
+    return None
 
   #=====================================================#
   # VITERBI ALGORITHM
@@ -36,102 +45,76 @@ class POSTagger():
     LEN_TOKENS = len(tokens)
     LEN_POSTAG = len(POS_TAGS)
 
-    print(tokens)
+    print(len(tokens), 'tokens like this: ', tokens)
 
-    # Initialize memo
+    # Initialize memo & backpointers for the best tags
+
+    # Visualize memo as a HMM network laid out
+    # Visualize best_postags as a HMM network laid out, denoting each viterbi
+    # node's best chosen backpointer to some previous viterbi node
     memo = []
+    best_postags = []
     for i in range(LEN_TOKENS):
       memo.append([])
+      best_postags.append([])
       for j in range(LEN_POSTAG):
-        memo[i].append(0)
+        memo[i].append(sys.float_info.min)
+        best_postags[i].append(-1)
 
-    for i in range(LEN_POSTAG):
-      # Probability at '<S>' is 1 since it occurs for all documents
-      memo[0][i] = 1
+        if i == 0:
+          # Initialize probability at '<S>' to 1 since it occurs for all documents
+          memo[i][j] = 0  # (log scale equivalent of probability = 1 is 0)
 
-    # Smoothing parameter
-    smooth_k = self.add_1_smoothing(tokens)
-
-    # Compute most probable path
-    best_indices = []
-    for i in range(LEN_POSTAG):
-      best_indices.append(0)
-
-    self.print_viterbi_state(memo, 4, tokens)
-
+    # Compute most probable path & store in memo and best_postags arrays
     for i in range(1, LEN_TOKENS):
       for j in range(LEN_POSTAG):
+        curr_max = -sys.float_info.max
+        back_ptr = -1
 
-        curr_max = 0
         for k in range(LEN_POSTAG):
-          # print(tokens[i], POS_TAGS[j], 'v:', memo[i - 1][j], ', P(t_i-1, t_i):', self.PROB_TAG_GIVEN_TAG[POS_TAGS[j]][POS_TAGS[j]], ', P(w_i, t_i):', self.PROB_WORD_GIVEN_TAG[POS_TAGS[j]][tokens[i]])
-          transition_prob = memo[i - 1][k] * self.PROB_TAG_GIVEN_TAG[POS_TAGS[k]][POS_TAGS[j]]
+          transition_prob = memo[i - 1][k] + \
+                            self.PROB_TAG_GIVEN_TAG[POS_TAGS[k]][POS_TAGS[j]] + \
+                            self.PROB_WORD_GIVEN_TAG[POS_TAGS[j]][tokens[i]]
 
           if transition_prob > curr_max:
             curr_max = transition_prob
+            back_ptr = k if i > 1 else 0
 
-        if tokens[i] in self.PROB_WORD_GIVEN_TAG[POS_TAGS[j]]:
-          # For seen words
-          memo[i][j] = curr_max * self.PROB_WORD_GIVEN_TAG[POS_TAGS[j]][tokens[i]]
-        else:
-          # For unseen words
-          # # TODO: Implement way to handle unseen tokens
-          memo[i][j] = curr_max * 0
+        memo[i][j] = curr_max
+        best_postags[i][j] = back_ptr
 
-    self.print_viterbi_state(memo, len(tokens), tokens)
-    # print(best_indices)
+    best_postag_at_end_of_sentence = self.find_arg_of_max(memo[LEN_TOKENS - 1])
+    found_best_postag_value = best_postag_at_end_of_sentence[0]
+    found_best_postag_index = best_postag_at_end_of_sentence[1]
+
+    print(self.get_best_viterbi_path(best_postags, found_best_postag_index))
 
     return None
 
-  # res = 'Labor: '
-  # headers = ' '
-  # for postag in POS_TAGS:
-  #   headers = headers + str(postag) + ' '
-  #   res = res + str(self.PROB_WORD_GIVEN_TAG[postag]['Labor']) + ' '
-  # print(headers)
-  # print(res)
+  def get_best_viterbi_path(self, back_ptrs, best_end_of_sentence_back_ptr):
+    # to traverse a sentence from last POS tag to 1st POS tag
+    # Note: ignore the 1st tag since its back ptr is undefined (i.e. there's no
+    #       reasonable back ptr for the 1st tag of a sentence)
+    back_ptrs = list(reversed(back_ptrs[1:]))
+    LEN_BACK_PTRS = len(back_ptrs)
 
-  def print_viterbi_state(self, memo, print_till_token_pos, tokens):
-    for i in range(len(tokens)):
-      if i < print_till_token_pos:
-        print_result = tokens[i] + ' '
-        for j in range(len(POS_TAGS)):
-          print_result = print_result + str(memo[i][j]) + ' '
-        print(print_result)
+    # last POS TAG is always an END_MARKER
+    # we can say this because we always add a START_MARKER & END_MARKER between
+    # sentences during the tokenization phase of the test set
+    best_pos_tag_sequence = [END_MARKER]
 
-  #=====================================================#
-  # TODO: SMOOTHING
-  # GOAL:
-  #   * Ensure probability of unseen words are not 0, or else, HMM will not work
-  #
-  # STRATEGY:
-  #   * Treat unseen words as though we've only seen them once
-  #     To penalize them even further, we could even make that probability only half as much, etc.
-  #=====================================================#
-  def add_1_smoothing(self, tokens):
-    unseen_count_pair = self.count_unseen_words(tokens)
-    vocab_size = self.count_seen_words() + unseen_count_pair[0]
-    seen_unseen_tokens = [unseen_count_pair[1], self.PROB_WORD_GIVEN_TAG[START_MARKER].keys()]
-    trained_tokens = [item for sublist in seen_unseen_tokens for item in sublist]
-    return 1 / len(trained_tokens)
+    for i in range(LEN_BACK_PTRS): # traversing sentence backwards
+      best_pos_tag_sequence.append(POS_TAGS[back_ptrs[i][best_end_of_sentence_back_ptr]])
+      best_end_of_sentence_back_ptr = back_ptrs[i][best_end_of_sentence_back_ptr]
 
-  def count_seen_words(self):
-    return len(self.PROB_WORD_GIVEN_TAG[START_MARKER].keys())
-
-  def count_unseen_words(self, tokens):
-    count = 0
-    unseens = []
-    for token in tokens:
-      if token not in self.PROB_WORD_GIVEN_TAG[START_MARKER]:
-        count += 1
-        unseens.append(token)
-    return [count, unseens]
+    # remember to reverse sequence of best POS tags since we traversed sentence backwards
+    return list(reversed(best_pos_tag_sequence))
 
   #=====================================================#
   # GENERATE TOKENS
   #=====================================================#
-  def generate_tokens_for_test_doc_sentences(self, sentences):
-    sen_as_tokens_list = [self.tokenizer.tokenize_test_document(sentence) for sentence in sentences]
+  def generate_tokens_for_test_doc_sentences(self, sentences, word_vocab):
+    sen_as_tokens_list = [self.tokenizer.tokenize_test_document(sentence, word_vocab) for sentence in sentences]
     sen_as_tokens_list = [sen_tokens for sen_tokens in sen_as_tokens_list if sen_tokens != []] # remove any empty lists due to empty sentences
     return sen_as_tokens_list
 
@@ -144,6 +127,20 @@ class POSTagger():
 
   def load_model(self):
     return pickle.load(open(PATH_TO_DATA_MODEL, 'rb'))
+
+  #=====================================================#
+  # HELPER METHODS
+  #=====================================================#
+  def find_arg_of_max(self, lst):
+    curr_max = -sys.float_info.max
+    curr_max_index = -1
+
+    for i in range(len(lst)):
+      if lst[i] > curr_max:
+        curr_max = lst[i]
+        curr_max_index = i
+
+    return (curr_max, curr_max_index)
 
 #=====================================================#
 # EXECUTION OF PROGRAM
